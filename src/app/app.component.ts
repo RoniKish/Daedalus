77e5292315669cc2aed6803dc994304b359588c2
RoniKish <R_k@gmail.com>
2019-10-11 19:27:01 +0300

    First working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DateSum';
+
+  daysSum;
+  monthsSum;
+  yearsSum ;
+
+  constructor() {}
+
+  datesFormGroup = new FormGroup ({
+    datesFormArray: new FormArray ([
+      new FormGroup({
+        days: new FormControl(0),
+        months: new FormControl(0),
+        years: new FormControl(0),
+      }),
+      new FormGroup({
+        days: new FormControl(0),
+        months: new FormControl(0),
+        years: new FormControl(0),
+      })
+    ])
+  })
+
+  onSubmit() {
+    this.daysSum = 0;
+    this.monthsSum = 0;
+    this.yearsSum = 0;
+
+    for (let date of this.datesFormGroup.value.datesFormArray) {
+      this.daysSum += date.days;
+      this.monthsSum += date.months;
+      this.yearsSum += date.years;
+    }
+
+    this.roundDaysSum();
+    this.roundMonthsSum();
+  }
+
+  roundDaysSum(){
+    this.monthsSum += Math.floor(this.daysSum/30);
+    this.daysSum = Math.floor(this.daysSum%30);
+  }
+
+  roundMonthsSum(){
+    this.yearsSum += Math.floor(this.monthsSum/12);
+    this.monthsSum = Math.floor(this.monthsSum%12);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// material design
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatCardModule } from '@angular/material'
+
+// forms
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // meterial design
+    BrowserAnimationsModule, MatInputModule, MatButtonModule, MatSelectModule, MatCardModule,
+    // forms
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
